c9d44ed50d8cc2b0c7214599fe306f231534ef59
Leonel Oliveira <leoneloliver@gmail.com>
2016-12-27 12:28:16 -0500

    Update tricks.py

--- tricks.py
+++ tricks.py
@@ -99,5 +99,19 @@ print(my_list)
 str = "THIS IS STRING EXAMPLE....WOW!!!"
 print str.lower()
 
+#example 17
+#!/usr/bin/python
+import re
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print "matchObj.group() : ", matchObj.group()
+   print "matchObj.group(1) : ", matchObj.group(1)
+   print "matchObj.group(2) : ", matchObj.group(2)
+else:
+   print "No match!!"
 
 
